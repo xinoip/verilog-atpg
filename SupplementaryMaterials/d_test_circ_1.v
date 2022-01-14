c1ea014fd764676189cabb67f2a09f9bb001f511
xinoip <pionix@protonmail.com>
2022-01-14 22:22:04 +0300

    material fix

--- SupplementaryMaterials/d_test_circ_1.v
+++ SupplementaryMaterials/d_test_circ_1.v
@@ -8,8 +8,8 @@ wire N28, N29, N30, N33;
 
 and AND2_8 (N28, N0, N1);
 nand NAND2_9 (N29, N1, N2);
-or NOR2_10 (N30, N28, N29);
-nor NOR2_11 (N33, N3, N4);
+or OR2_10 (N30, N28, N29);
+or OR2_11 (N33, N3, N4);
 and AND2_12 (N14, N33, N30);
 and AND2_13 (N15, N30, N5);
 
